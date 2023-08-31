1693ede38c1589f5f180e3b8108d57a32970bbc3
unknown <brannongarrett77@gmail.com>
2023-08-31 15:28:15 +0200

    Added multiple variables and code

--- mad-libs-generator.py
+++ mad-libs-generator.py
@@ -1,3 +1,12 @@
-def main():
+with open("story.txt", "r") as f:
+    story = f.read()
 
-main()
+words = []
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
