aef82be9b6f262a7c2e1be475f8f4b04ffc37086
unknown <brannongarrett77@gmail.com>
2023-09-03 20:13:05 +0200

    Finished the app

--- mad-libs-generator.py
+++ mad-libs-generator.py
@@ -21,4 +21,7 @@ for word in words:
     answer = input("Enter a word for " + word + ": ")
     answers[word] = answer
 
-print(answers)
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story)
